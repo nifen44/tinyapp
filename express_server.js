8383a04da7acdc47900bfdc460720481ec458153
Wei Huang <huangwei19810803@gmail.com>
2023-05-15 17:56:23 -0600

    add urls_show and urls_index and header

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -16,8 +18,21 @@ app.get("/urls.json", (req, res)=>{
 });
 
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+    const templateVars = { greeting: "Hello World!"};
+    res.render('hello_world', templateVars);
   });
+
+app.get('/urls', (req, res)=>{
+    const templateVars = {urls: urlDatabase};
+    res.render("urls_index", templateVars);
+})
+
+app.get('/urls/:id', (req, res)=>{
+    const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+    if(templateVars){
+        res.render("urls_show", templateVars);
+    }
+})
   
 
 app.listen(PORT, () => {
