0081031f8f615d8ce726945ac20defb41a81822f
Wei Huang <huangwei19810803@gmail.com>
2023-05-15 20:39:15 -0600

    add redirect

--- express_server.js
+++ express_server.js
@@ -39,13 +39,21 @@ app.get('/urls/:id', (req, res)=>{
     }
 })
 
+app.get('/u/:id', (req, res)=>{
+    const longURL = urlDatabase[req.params.id];
+    res.redirect(longURL);
+})
+
 app.post("/urls", (req, res) => {
     console.log(req.body); // Log the POST request body to the console
-    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+    // res.send("Ok"); // Respond with 'Ok' (we will replace this)
 
-    //const randomID = generateRandomString(6);
+    const id = generateRandomString(6);
+    
+    urlDatabase[id] = req.body.longURL;
 
-
+    console.log(urlDatabase);
+    res.redirect(`/urls/${id}`);
   });
 
   
