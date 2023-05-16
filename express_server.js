3740891d78166b04c6445b90ab970a94d15f6f86
Wei Huang <huangwei19810803@gmail.com>
2023-05-16 14:24:22 -0600

    add set cookie

--- express_server.js
+++ express_server.js
@@ -65,6 +65,12 @@ app.post("/urls", (req, res) => {
     res.redirect("/urls")
   })
 
+  app.post("/login", (req, res)=>{
+    const { username } = req.body;
+    res.cookie('username', username);
+    res.redirect("/urls");
+  })
+
   
 
 app.listen(PORT, () => {
