52a1070551add18f0a9487165f0e054e3b0de04d
Wei Huang <huangwei19810803@gmail.com>
2023-05-17 17:38:30 -0600

    add helper.js and mocha

---
+++ test/helpersTest.js
@@ -0,0 +1,31 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    // Write your assert statement here
+    assert.equal(user.id, expectedUserID);
+  });
+
+  it('should return undefined when this email is not in our database', ()=>{
+    const user = getUserByEmail("user2222@example.com", testUsers);
+    const expectedUserID = undefined;
+    assert.equal(user, expectedUserID);
+  })
+});
