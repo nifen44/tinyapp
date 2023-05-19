62ca2dec861ce462e9f010430c6125d93893da95
Wei Huang <huangwei19810803@gmail.com>
2023-05-18 18:01:53 -0600

    modify user authentic and add docs folder

--- express_server.js
+++ express_server.js
@@ -2,16 +2,17 @@ const express = require("express");
 const app = express();
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
 const PORT = 8080; // default port 8080
 
-const { getUserByEmail } = require('./helpers');
+const { getUserByEmail, generateRandomString, goToCertifyPage, urlsForUser, isLoggedIn, authenticateUser } = require('./helpers');
 
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({
   name:'session',
-  keys: ['key1', 'key2'],
+  keys: ['my favorite thing', 'learning'],
 }));
 
 const urlDatabase = {
@@ -23,254 +24,217 @@ let users = {};
 
 // go to index page
 app.get('/urls', (req, res)=>{
-  if(!req.session.user_id || req.session.user_id === ''){
-    goToCertifyPage(res, 'login');
-  }else{
-    for(const id in users){
-      if(users[id].id === req.session.user_id){
-        const currentUserUrls = urlsForUser(req.session.user_id);
-        console.log(currentUserUrls);
+  if (isLoggedIn(req.session.user_id)) {
+    // didn't Logged in
+    goToCertifyPage(res, 'login', urlDatabase);
+  } else {
+    // Logged in
+    for (const id in users) {
+      // find the specific user
+      if (users[id].id === req.session.user_id) {
+        const currentUserUrls = urlsForUser(req.session.user_id, urlDatabase);
+        //console.log(currentUserUrls);
         const templateVars = {
           user: users[req.session.user_id],
           urls: currentUserUrls,
-        }
+        };
         return res.render("urls_index", templateVars);
       }
     }
-    goToCertifyPage(res, 'login');
+    // didn't find the user from users
+    goToCertifyPage(res, 'login', urlDatabase);
   }
     
-})
+});
 
-// go to create page
+// go to create url page
 app.get('/urls/new', (req, res)=>{
-  if(!req.session.user_id || req.session.user_id === ''){
+  if (isLoggedIn(req.session.user_id)) {
     return res.redirect('/login');
-
   }
   const templateVars = {
     user: users[req.session.user_id],
-  }
-    return res.render('urls_new', templateVars);
-})
+  };
+  return res.render('urls_new', templateVars);
+});
 
 // go to link detail page
 app.get('/urls/:id', (req, res)=>{
 
   //POST /urls/:id should return a relevant error message if the user is not logged in
-  if(!req.session.user_id || req.session.user_id === ''){
+  if (isLoggedIn(req.session.user_id)) {
     return res.send("you haven't login, can not access this page");
   }
 
   //POST /urls/:id should return a relevant error message if id does not exist
-
   //POST /urls/:id should return a relevant error message if the user does not own the URL
-  const currentUserUrls = urlsForUser(req.session.user_id);
-  if(currentUserUrls[req.params.id] && currentUserUrls[req.params.id]!== undefined){
-      const templateVars = {
-        id: req.params.id, 
-        longURL: urlDatabase[req.params.id].longURL,
-        user: users[req.session.user_id],
-      }
+  const currentUserUrls = urlsForUser(req.session.user_id, urlDatabase);
+  if (currentUserUrls[req.params.id] && currentUserUrls[req.params.id] !== undefined) {
+    const templateVars = {
+      id: req.params.id,
+      longURL: urlDatabase[req.params.id].longURL,
+      user: users[req.session.user_id],
+    };
   
-      if(templateVars){
-          return res.render("urls_show", templateVars);
-      }
-  }else{
+    if (templateVars) {
+      return res.render("urls_show", templateVars);
+    }
+  } else {
     res.send('you can not access this url');
   }
   
-})
+});
 
- // go to real link page by using shortURL
+// go to real link page by using shortURL
 app.get('/u/:id', (req, res)=>{
-    const longURL = urlDatabase[req.params.id].longURL;
-    res.redirect(longURL);
-})
+  const longURL = urlDatabase[req.params.id].longURL;
+  res.redirect(longURL);
+});
 
-  // go to edit page
+// go to edit page
 app.get('/urls/:id/edit', (req, res)=>{
   const editId = req.params.id;
   res.redirect(`/urls/${editId}`);
-})
+});
 
- // create new link
+// create new link
 app.post("/urls", (req, res) => {
-  if(!req.session.user_id || req.session.user_id === ''){
+  if (isLoggedIn(req.session.user_id)) {
     return res.sendStatus(403);
-  }else{
-    for(const id in users){
-      if(users[id].id === req.session.user_id){
+  } else {
+    for (const id in users) {
+      if (users[id].id === req.session.user_id) {
         const id = generateRandomString(6);
         urlDatabase[id] = {
           longURL: req.body.longURL,
           userID: req.session.user_id
         };
-        console.log(urlDatabase);
+        //console.log(urlDatabase);
         res.redirect(`/urls/${id}`);
       }
     }
   }
-    console.log(req.body); // Log the POST request body to the console
-    // res.send("Ok"); // Respond with 'Ok' (we will replace this)
-
+  //console.log(req.body); 
     
-  });
+});
 
-  // delete link
-  app.get("/urls/:id/delete", (req, res)=>{
-    console.log(req.params.id);
-    //POST /urls/:id/delete should return a relevant error message if the user is not logged in
-    if(!req.session.user_id || req.session.user_id === ''){
-      return res.send("you haven't login, can not access this page");
-    }
+// delete link
+app.get("/urls/:id/delete", (req, res)=>{
+  console.log(req.params.id);
+  //POST /urls/:id/delete should return a relevant error message if the user is not logged in
+  if (isLoggedIn(req.session.user_id)) {
+    return res.send("you haven't login, can not access this page");
+  }
 
-    //POST /urls/:id/delete should return a relevant error message if id does not exist
-    //POST /urls/:id/delete should return a relevant error message if the user does not own the URL.
-    const currentUserUrls = urlsForUser(req.session.user_id);
-    if(currentUserUrls[req.params.id] && currentUserUrls[req.params.id]!== undefined){
-      const deleteId = req.params.id;
-      delete urlDatabase[deleteId];
-      res.redirect("/urls");
-    }else{
-      res.send("No such url");
-    }
-    
+  //POST /urls/:id/delete should return a relevant error message if id does not exist
+  //POST /urls/:id/delete should return a relevant error message if the user does not own the URL.
+  const currentUserUrls = urlsForUser(req.session.user_id, urlDatabase);
+  if (currentUserUrls[req.params.id] && currentUserUrls[req.params.id] !== undefined) {
+    const deleteId = req.params.id;
+    delete urlDatabase[deleteId];
+    res.redirect("/urls");
+  } else {
+    res.send("No such url");
+  }
+});
 
-    
-  })
+// edit link
+app.post('/urls/:id/edit', (req, res)=>{
+  const { id } = req.params;
+  const { longURL } = req.body;
 
-  // edit link
-  app.post('/urls/:id/edit', (req, res)=>{
-    const { id } = req.params;
-    const { longURL } = req.body;
+  // console.log(`${id}, ${longURL}`);
+  // console.log(`------${urlDatabase}------`);
+  urlDatabase[id].longURL = longURL;
+  res.redirect("/urls");
+});
 
-    console.log(`${id}, ${longURL}`);
-    console.log(`------${urlDatabase}------`);
-    urlDatabase[id].longURL = longURL;
-    res.redirect("/urls")
-  })
-
-    // go to login page
-    app.get('/login', (req, res)=>{
-      if(!req.session.user_id || req.session.user_id === ''){
-        goToCertifyPage(res, 'login');
-      }else{
-        // cookie is exist, we need to check if the cookie matches one of the user in users
-        for(const id in users){
-          if(users[id].id === req.session.user_id){
-            return res.redirect('/urls');
-          }
-        }
-        goToCertifyPage(res, 'login');
+// go to login page
+app.get('/login', (req, res)=>{
+  if (isLoggedIn(req.session.user_id)) {
+    goToCertifyPage(res, 'login',  urlDatabase);
+  } else {
+    // cookie is exist, we need to check if the cookie matches one of the user in users
+    for (const id in users) {
+      if (users[id].id === req.session.user_id) {
+        return res.redirect('/urls');
       }
-      
-    })
-
-  // login
-  app.post("/login", (req, res)=>{
-    const { email, password } = req.body;
-    const user = getUserByEmail(email, users);
-    if(user){
-      // check password
-      if(!bcrypt.compareSync(password, user.password)){
-        return res.sendStatus(403);
-      }else{
-        // console.log(`login success: ${user.email}`);
-        req.session.user_id = user.id;
-        res.redirect('/urls');
-      }
-      
-    }else{
-      return res.sendStatus(403);
     }
-  })
+    goToCertifyPage(res, 'login', urlDatabase);
+  }
+});
+
+// login
+app.post("/login", (req, res)=>{
+  const { email, password } = req.body;
+  const { err, user } = authenticateUser(email, password, users);
+
+  if(err){
+    return res.json(err);
+  }
+
+  req.session.user_id = user.id;
+  return res.redirect('/urls');
+});
 
 
-  // logout 
-  app.get("/logout", (req, res)=>{
-    req.session.user_id = null;
-    res.redirect('/login');
-  })
+// logout
+app.get("/logout", (req, res)=>{
+  req.session.user_id = null;
+  res.redirect('/login');
+});
 
-  // go to register page
-  app.get('/register', (req, res)=>{
-    if(!req.session.user_id || req.session.user_id === ''){
-      goToCertifyPage(res, 'registration');
-    }else{
-      // cookie is exist, we need to check if the cookie matches one of the user in users
-      for(const id in users){
-        if(users[id].id === req.session.user_id){
-          return res.redirect('/urls');
-        }
+// go to register page
+app.get('/register', (req, res)=>{
+  if (isLoggedIn(req.session.user_id)) {
+    goToCertifyPage(res, 'registration', urlDatabase);
+  } else {
+    // cookie is exist, we need to check if the cookie matches one of the user in users
+    for (const id in users) {
+      if (users[id].id === req.session.user_id) {
+        return res.redirect('/urls');
       }
-      goToCertifyPage(res, 'registration');
     }
+    goToCertifyPage(res, 'registration', urlDatabase);
+  }
     
-  })
+});
 
-  // register user
-  app.post('/register', (req, res)=>{
-    const userId = generateRandomString(6);
-    const { email, password } = req.body;
+// register user
+app.post('/register', (req, res)=>{
+  const userId = generateRandomString(6);
+  const { email, password } = req.body;
 
-    if(!email || !password){
-      return res.sendStatus(400);
-    }
+  if (!email || !password) {
+    return res.sendStatus(400);
+  }
 
-    if(getUserByEmail(email, users)){
-      // user has already exist
-      console.log('--user exist--')
-      return res.sendStatus(400);
-    }
+  if (getUserByEmail(email, users)) {
+    // user has already exist
+    console.log('--user exist--');
+    return res.sendStatus(400);
+  }
 
-    const hashedPassword = bcrypt.hashSync(password, 10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
 
-    users[userId] = {
-      id: userId,
-      email,
-      password: hashedPassword,
-    }
+  users[userId] = {
+    id: userId,
+    email,
+    password: hashedPassword,
+  };
 
-    console.log(Object.entries(users));
+  //console.log(Object.entries(users));
 
-    req.session.user_id = userId;
+  req.session.user_id = userId;
 
-    res.redirect('/urls');
-  })
+  res.redirect('/urls');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-function generateRandomString(length){
-    const alphanumeric = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
 
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * alphanumeric.length);
-        result += alphanumeric.charAt(randomIndex);
-    }
 
-    return result;
-}
 
-function goToCertifyPage(res, page){
-  const templateVars = {
-    user: null,
-    urls: urlDatabase
-  }
-  return res.render(page, templateVars);
-}
-
-function urlsForUser(userId){
-  let userUrls = {};
-  for(const id in urlDatabase){
-    if(urlDatabase[id].userID === userId){
-      userUrls[id] = urlDatabase[id];
-    }
-  }
-
-  return userUrls;
-}
 
--- helpers.js
+++ helpers.js
@@ -1,11 +1,69 @@
-function getUserByEmail(email, database){
-    for(const id in database){
-      if(database[id].email === email){
-        return database[id];
-      }
+const bcrypt = require('bcryptjs');
+
+const getUserByEmail = (email, database)=>{
+  for (const id in database) {
+    if (database[id].email === email) {
+      return database[id];
     }
+  }
   
-    return undefined;
+  return undefined;
+};
+
+const generateRandomString = (length)=>{
+  const alphanumeric = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * alphanumeric.length);
+    result += alphanumeric.charAt(randomIndex);
   }
 
-  module.exports = { getUserByEmail };
+  return result;
+};
+
+const goToCertifyPage = (res, page, database)=>{
+  const templateVars = {
+    user: null,
+    urls: database
+  };
+  return res.render(page, templateVars);
+};
+
+const urlsForUser = (userId, database)=>{
+  let userUrls = {};
+  for (const id in database) {
+    if (database[id].userID === userId) {
+      userUrls[id] = database[id];
+    }
+  }
+
+  return userUrls;
+};
+
+const isLoggedIn = (id)=>{
+  if (!id || id === '') return true;
+  return false;
+}
+
+const authenticateUser = (email, password, users)=>{
+  let currentUser = null;
+
+  for(const userId in users){
+    if(users[userId].email === email){
+      currentUser = users[userId];
+    }
+  }
+
+  if(!currentUser){   // Eject clauses
+      return {err: 'No valid user', user: null}
+  }
+
+  if(!bcrypt.compareSync(password, currentUser.password)){  // Eject clauses
+      return {err: 'Email or Password invalid', user: null};
+  }
+
+  return {err: null, user: currentUser};
+}
+
+module.exports = { getUserByEmail, generateRandomString, goToCertifyPage,urlsForUser, isLoggedIn, authenticateUser };
